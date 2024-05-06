494f39be91b870d980e06d14ff2c7e40a9ab5798
Ananthu  M V <ananthu.mv8@gmail.com>
2024-05-06 10:06:55 +0530

    movielist first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import react, {useState} from 'react'
 
 function App() {
+  const [director, setDirector] = useState('');
+  const [movie, setMovie] = useState('');
+  const [movieList, setMovieList] = useState([]);
+
+  const addMovie =() =>{
+      setMovieList([...movieList, {director, movie}]);
+  }
+
+  const removeMovie = (index) =>{
+    const update = movieList.filter((item, i) => i !== index);
+    setMovieList(update);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <table>
+        <thead>
+           <th></th>
+           <th></th>
+        </thead>
+        <tr>
+            <td>
+                <input type="text" value={director} onChange={(e) => setDirector(e.target.value)} placeholder="Director" />
+            </td>
+            <td>
+              <input type="text"  value={movie} onChange={(e) => setMovie(e.target.value)} placeholder="Movie Name" />
+            </td>
+            <td>
+                <button onClick={addMovie}>Add</button>
+            </td>
+        </tr>
+         <thead> 
+          <th>Director</th>
+            <th>Movie</th> 
+          </thead>
+          {movieList.map((item, index)=>(
+            <tr key={index}>   
+              <td>{item.director}</td>
+              <td>{item.movie}</td>
+              <td>
+
+                <button onClick={() => removeMovie(index)}>
+                  remove
+                </button>
+              </td>
+              
+            </tr>
+          ))}
+         
+      </table>
     </div>
   );
 }
